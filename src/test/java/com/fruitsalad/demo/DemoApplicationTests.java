d80258bfb67e73a9a0cf0ec769ec5ca1fd28a819
wzh <wzhlink@163.com>
2019-07-15 14:46:41 +0800

    init

---
+++ src/test/java/com/fruitsalad/demo/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.fruitsalad.demo;
+
+import com.fruitsalad.demo.common.jdbc.JdbcTemplate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	public void contextLoads() {
+		jdbcTemplate.getConnection();
+	}
+
+}
